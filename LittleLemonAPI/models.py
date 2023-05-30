350b432ad787b78d2b362486710e5fea30c86946
theKwabena <abbeyemmanueltetteh@gmail.com>
2023-05-30 12:49:29 +0000

    capstone final

--- LittleLemonAPI/models.py
+++ LittleLemonAPI/models.py
@@ -6,5 +6,8 @@ class Menu(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.IntegerField()
 
+    def __str__(self):
+        return f'{self.title} : {self.price}'
+
     def get_item(self):
         return f'{self.title} : {str(self.price)}'
